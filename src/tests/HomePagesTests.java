ae12eec053efe3435a6b0c6f9485f80b58340b28
Bronit <bronitskayaelena@gmail.com>
2018-12-26 16:28:47 +0200

    1.Class "HomePageTests" was refactored.New package "pages" with 3 classes: "EventsUnAythPageHelper", "HomePageHelper", "PageBase" were created. 2. Class "AccountCreatePageTests" was corrected with corrections in class "TestBase": method of creation random email and methods of browser download were changed.

--- src/tests/HomePagesTests.java
+++ src/tests/HomePagesTests.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import pages.EventsUnAuthPageHelper;
+import pages.HomePageHelper;
 
 import java.util.List;
 
@@ -15,48 +19,28 @@ import java.util.List;
  * Created by Inka on 16-Dec-18.
  */
 public class HomePagesTests extends TestBase {
-    @Test
-    public void openHomePage() throws InterruptedException {
-        //Thread.sleep(20000);
+    HomePageHelper homePage;
+    EventsUnAuthPageHelper eventsUnAuthPage;
 
-        waitUntilElementIsLoaded(driver,
-                By.xpath("//span[contains(text(),'Login')]"),
-                45);
-        List<WebElement> topLinksList = driver.findElements(By
-                .cssSelector(".mat-button-wrapper .mat-button-wrapper"));
-        System.out.println("Login:" + topLinksList.get(0).getText());
-        System.out.println("Create Account:"
-                + topLinksList.get(1).getText());
-        WebElement goToEventsButton = driver.findElement(
-                By.className("mat-stroked-button"));
-        //System.out.println("Text of button: "
-        // + goToEventsButton.getText());
-        //Thread.sleep(5000);
-        Assert.assertTrue(goToEventsButton.getText()
-                .equals("Go to Event list"));
+    @BeforeMethod
+    public void initPage(){
+        homePage = PageFactory.initElements(driver, HomePageHelper.class);
+        eventsUnAuthPage = PageFactory.initElements(driver, EventsUnAuthPageHelper.class);
+    }
+    @Test
+    public void openHomePage() {
+        homePage.waitUntilPageLoad();
+        String goToButtonName = homePage.getGoToEventButtonName();
+        Assert.assertEquals("Go to Event list", goToButtonName);
     }
 
     @Test
-    public void goToEventsTest() throws InterruptedException {
-        Thread.sleep(10000);
-        List<WebElement> topLinksList = driver.findElements(By.cssSelector(".mat-button-wrapper .mat-button-wrapper"));
-        System.out.println("Login:" + topLinksList.get(0).getText());
-        System.out.println("Create Account:" + topLinksList.get(1).getText());
-        waitUntilElementIsLoaded(driver,
-                By.xpath("//span[contains(text(),'Login')]"),
-                45);
-        WebElement goToEventsButton = driver.findElement(
-                By.className("mat-stroked-button"));
-        System.out.println("Text of button: " + goToEventsButton.getText());
-        Thread.sleep(5000);
-        goToEventsButton.click();
-        Thread.sleep(5000);
-        waitUntilElementIsLoaded(driver,
-                By.xpath("//span[contains(text(),'Filters')]"),
-                20);
-        WebElement titlePage = driver.findElement(By
-                .xpath("//h1[@class='gorisontal-center']"));
-        Assert.assertTrue(titlePage.getText().equals("Find event"));
+    public void goToEventsTest() {
+        homePage.waitUntilPageLoad();
+        homePage.pressGoToEventButton();
+        eventsUnAuthPage.waitUntilPageLoad();
+        String header = eventsUnAuthPage.getHeader();
+        Assert.assertEquals(" Find event", header);
     }
 
     }
--- src/tests/TestBase.java
+++ src/tests/TestBase.java
@@ -5,18 +5,23 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.Random;
 
 public class TestBase {
     WebDriver driver = new ChromeDriver();
     @BeforeMethod
     public void initWebDriver(){
+      //  driver = new ChromeDriver();
         driver.get("https://mish.sheygam.com/#/wellcome");
         driver.manage().window().maximize();
     }
 
-    @AfterSuite
+    //@AfterSuite
+    @AfterMethod
     public void tearDown(){
         driver.quit();
     }
@@ -31,5 +36,21 @@ public class TestBase {
         catch(Exception e){
             e.printStackTrace();
         }
+    }
+
+    public static String latinDigitString(int length){
+        String str = "";
+        char ch;
+        int number;
+        Random gen = new Random();
+        int i = 0;
+        do {
+            number = '0'/*по табл. Аски это 48*/+ gen.nextInt('z' - '0' +1);//метод gen.nextInt(100)+1+20-посмотнть выбор рандомальных чисел от 20 до 120
+            if ((number <= '9') || (number >= 'A' && number <= 'Z') || (number >= 'a'))
+            {
+                str = str + (char)number;
+            }
+        }while(str.length()<length);
+        return str;
     }
 }
